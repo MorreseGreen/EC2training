9617784758bf41b3c263730c82c6b4f666a0954b
mo-green <morrese.green@cloudreach.com>
2022-04-06 08:45:23 -0400

    rest of updated files for challenge 1 and challenge 2

--- security-groups.tf
+++ security-groups.tf
@@ -1,33 +1,36 @@
-recource "aws_security_group" "my_public_app_sg"{
- name = "my_public_app_sg"
- description = "Allow access to server"
- vpc_id = data.aws_vpc.main_vpc.id
+resource "aws_security_group" "my_public_app_sg" {
+  name        = "my_public_app_sg"
+  description = "Allow access to server"
+  vpc_id      = data.aws_vpc.main_vpc.id
+
+
+  #Inbound Connections
+  ingress {
+    description = "Allow SSH into the EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["38.88.10.210/32"]
+  }
+
+  # ingress {
+  #   description = "Allow HTTP into the EC2"
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["38.88.10.210/32"] # 0.0.0.0/0
+  # }
+
+
+  #Outbound Connections
+
+  egress {
+    description = "Allow access to all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # TCP + UDP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-#Inbound Connections
-ingress{
- description = "Allow SSH into the EC2"
- from_port = 22
- to_port = 22
- protocol = "tcp"
- cidr_blocks = ["38.88.10.210/32"]
-}
-
-ingress {
-  description = "Allow HTTP into the EC2"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["38.88.32.10.210/32"] # 0.0.0.0/0
-}
-
-
-#Outbound Connections
-
-egress{
- description = ""Allow access to all"
- from_port = 0
- to_port = 0
- protocol = "-1" # TCP + UDP
- cidr_blocks = ["0.0.0.0/0"]
-}
+#resource "aws_security_group" "my_private_app_sg"
